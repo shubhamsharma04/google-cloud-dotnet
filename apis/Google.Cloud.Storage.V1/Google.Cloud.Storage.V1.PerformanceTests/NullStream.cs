d9335e4a646b8b99b94a06aa1a576b2369023d57
Jon Skeet <jonskeet@google.com>
2017-08-01 15:33:35 +0100

    First pass at GCS download performance tester

---
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.PerformanceTests/NullStream.cs
@@ -0,0 +1,62 @@
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.IO;
+using System.Threading;
+
+namespace Google.Cloud.Storage.V1.PerformanceTests
+{
+    /// <summary>
+    /// Stream which remembers how much data has been written to it, but not the data itself.
+    /// </summary>
+    internal class NullStream : Stream
+    {
+        private long length;
+
+        public override bool CanRead => false;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => Interlocked.Read(ref length);
+
+        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Flush()
+        {
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            Interlocked.Add(ref length, count);
+        }
+    }
+}
---
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.PerformanceTests/Program.cs
@@ -0,0 +1,124 @@
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+using Google.Apis.Download;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Cloud.Storage.V1.PerformanceTests
+{
+    /// <summary>
+    /// Currently just a "download very fast" test. May become more useful over time.
+    /// Could it use xUnit etc? Not sure.
+    /// </summary>
+    class Program
+    {
+        private const string HundredMegFile = "100mb.dat";
+
+        // Horrible global state, but it's simpler than passing it around...
+        private static string bucket;
+        private static StorageClient client;
+        private static int iterations;
+        private static long totalData;
+        private static volatile bool done;
+
+        static int Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.Error.WriteLine("Arguments: <bucket> <iterations> <threads>");
+                Console.Error.WriteLine("The bucket must exist beforehand, but files will be generated if necessary.");
+                return 1;
+            }
+
+            client = StorageClient.Create();
+            bucket = args[0];
+            iterations = int.Parse(args[1]);
+            int threadCount = int.Parse(args[2]);
+
+            CreateFileIfNecessary();
+
+            var progressTask = Task.Run((Action) ReportProgress);
+            var workerThreads = Enumerable.Range(0, threadCount).Select(_ => Task.Run((Action) Download)).ToArray();
+            Task.WaitAll(workerThreads);
+            done = true;
+            progressTask.Wait();
+
+            return 0;
+        }
+
+        private static void Download()
+        {
+            var stream = new NullStream();
+            for (int i = 0; i < iterations; i++)
+            {
+                client.DownloadObject(bucket, HundredMegFile, stream,
+                    new DownloadObjectOptions { ChunkSize = 1 * 1024 * 1024 }, // 1MB chunks
+                    new ProgressMonitor());
+            }
+        }
+
+        private static void ReportProgress()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            while (!done)
+            {
+                Thread.Sleep(1000);
+                long ms = sw.ElapsedMilliseconds;
+                long bytesRead = Interlocked.Read(ref totalData);
+                double speed = (bytesRead / (1024.0 * 1024.0)) / (ms / 1000.0);
+                Console.WriteLine($"{speed:F2}MBps     ({bytesRead} bytes in {ms}ms)");
+            }
+        }
+
+        private static void CreateFileIfNecessary()
+        {
+            try
+            {
+                client.GetObject(bucket, HundredMegFile);
+                Console.WriteLine($"File {bucket}/{HundredMegFile} already exists. Skipping upload.");
+                return;
+            }
+            catch (GoogleApiException e) when (e.HttpStatusCode == HttpStatusCode.NotFound)
+            {
+            }
+            Console.WriteLine($"File {bucket}/{HundredMegFile} not found. Creating.");
+            var data = new byte[100 * 1024 * 1024];
+            new Random().NextBytes(data);
+            var stream = new MemoryStream(data);
+            var stopwatch = Stopwatch.StartNew();
+            client.UploadObject(bucket, HundredMegFile, "application/octet-stream", stream);
+            stopwatch.Stop();
+            Console.WriteLine($"100MB uploaded in {stopwatch.ElapsedMilliseconds}ms");
+        }
+
+        private class ProgressMonitor : IProgress<IDownloadProgress>
+        {
+            private long lastSeen = 0;
+
+            public void Report(IDownloadProgress value)
+            {
+                long current = value.BytesDownloaded;
+                Interlocked.Add(ref totalData, current - lastSeen);
+                lastSeen = current;
+            }
+        }
+    }
+}
